2cbcddf610672b46c6cf8da34d19f8816f9a59bb
DATAUNIRIO <duttross@gmail.com>
2022-11-19 13:45:08 -0300

    nova versao da apresentacao

---
+++ googlenews/anotacoes_para_roteiro_e_artigo.R
@@ -0,0 +1,40 @@
+# Experimento e Resultados
+# Nesta seção, apresentamos a nossa abordagem contendo três elementos.
+#Em primeiro lugar, a técnica de pré-processamento.
+#Em seguida, descrevemos o corpus e o léxico, então, finalmente, avaliamos os modelos e os resultados encontrados.
+
+# Coleta de dados via Google News, blablabla
+# O Corpus (conjunto de documentos sobre determinado tema)
+#4.353 matérias de jornais, revistas e blogs sobre a Lei Aldir Blanc
+#2.626 matérias únicas e exclusivas de jornais, revistas e blogs sobre a Lei Aldir Blanc
+
+# O léxico
+#Utilizamos neste trabalho o OpLexicon - um léxico de sentimento para a língua portuguesa
+#medida construída usando múltiplas fontes de informação (Silas Gonzaga, 2017). O léxico é constituído
+#de cerca de 32.000 palavras polarizadas classificadas por sua categoria morfológica
+#notado com polaridades positivo, negativo e neutro. 
+
+# Discussão e conclusão
+
+# Referências 
+# GONZAGA, S. lexiconPT: Lexicons for Portuguese Text Analysis, 2017;
+# SILGE, J.; RODINSON, D. Text Mining with R: A tidy approach. 1st ed. O’Reilly Media, 2017.
+
+
+# Medidas descritivas para texto.
+# Índices de diversidade, abundância e riqueza.
+# Bag of words (BOW) e NLP.
+# Termos frequentes, associações e redes de relacionamento.
+# Dendogramas, núvem de palavras.
+# Análise de sentimentos.
+# Aplicação de acervos léxicos.
+# Análise de agrupamento.
+# Médidas de distância e similaridade.
+# K-médias e variações.
+# Modelagem de tópicos.
+# Latent Dirichlet allocation.
+# Abordagens text2vec.
+# Modelagem preditiva apoiada em texto.
+# Classificação e predição.
+
+
