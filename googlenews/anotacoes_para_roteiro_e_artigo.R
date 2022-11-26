ab698ff62ae6028d655f4887adb0e09e65e24010
DATAUNIRIO <duttross@gmail.com>
2022-11-25 21:03:59 -0300

    ppt

--- googlenews/anotacoes_para_roteiro_e_artigo.R
+++ googlenews/anotacoes_para_roteiro_e_artigo.R
@@ -38,3 +38,45 @@
 # Classificação e predição.
 
 
+
+
+
+
+
+
+
+# a realidade para as pessoas é aquilo que as pessoas constroem como sendo real e elas o fazem na maioria das vezes através do texto e da conversação.
+# O papel da linguagem nas Ciências Sociais
+#Que vão bem mais além do que simples aumento da ênfase dada a importância da linguagem ele contribuiu? Para que fossem esboçados novos conceitos?
+#  o impacto da linguística estrutural
+# Escrever é também fazer.
+# Mais do que meramente descritiva, a linguagem nas notícias se institui como uma ação sobre o mundo
+# não se limita a refletir as coisas do mundo, mas também atuando sobre elas participando da sua construção.
+# O giro linguístico teve efeito e implicações.
+#Em outras palavras, o meu eu não é independente de como eu o vivêncio... como eu interpreto linguisticamente
+#ao contrário é o resultado dessa interpretação.
+
+#o giro linguístico, portanto, substitui a relação ideias/mundo. pela relação linguagem/mundo 
+#Com o giro linguístico também surge a possibilidade da ação científica ser considerada como uma prática social e quivalente a qualquer outro tipo de ação social.
+#Entender o contexto social da LAB, mas como também de se escreve, fala, e constitui a narrativa
+#a etnometodologia sintetiza magistralmente os princípios básicos que permitem Inserir a linguagem como elemento chave na análise e na compreensão da vida da estrutura social e além disso os levar a prática teoria teórica e metodologicamente.
+#Podemos destacar que é uma nova perspectiva a partir do qual podemos analisar os processos sociais.
+#método para avaliar a linguagem cotidiana - uma maneira de relatar a realidade
+#atribuindo a função da linguagem cotidiana
+#X na página 65
+#ação social estrutura social linguagem institucional
+#a concepção de discurso
+#O discurso é algo mais do que apenas a fala algo a mais haverá um conjunto de enunciados, o discurso é uma prática e como no caso de qualquer prática social é possível definir as condições de sua produção. 
+
+#Todo discurso tem um contexto de produção.
+#Esse contexto é a formação discursiva.
+#Os discursos são práticas sociais.
+
+#Aborda a linguagem de uma maneira radicalmente diferente de outras perspectivas.
+
+#análise crítica do discurso
+#Qualquer texto constitui um discurso?
+#Nem todos os textos podem ser considerados discursos. 
+#Para um texto ser efetivamente um discurso é necessário que cumpra certas condições
+# 1. aqueles que possuem valores para coletividade
+# 2. Que envolve crenças e convicções compartilhadas.
